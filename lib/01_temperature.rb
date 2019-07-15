515885d51a2256a7305927fce14ea368333df27a
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-15 13:50:36 +0200

    Mise à jour du fichier 01_temperature

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,11 +1,14 @@
+#Transforme des Fahrenheit en degré Celsius (formule trouvé en posant un système)
 
-#Transforme des Fahrenheit en degré Celsius 
 def ftoc(temperature)
-    return ((temperature.to_f - 32)*5/9).round(1)
+    return ((temperature.to_f - 32)*5/9)
 end
 
+
 #Transforme des degrés Celsius en Fahrenheit  
+
 def ctof(temperature)
-    return (temperature.to_f*9/5+32).round(1)
+    return (temperature.to_f*9/5+32)
+
 end
 
